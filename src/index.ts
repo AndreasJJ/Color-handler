b8a8495b4a51a3a97a716b3090cdc97ae979346c
Andreasjj <andreasjj@gmail.com>
2020-10-10 18:38:32 +0200

    updated exports

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import rgba2rgb, { rgba2rgbString, rgba2rgbObject, rgba2rgbArray } from './rgbaT
 import hex2rgb, { hex2rgbString, hex2rgbObject, hex2rgbArray } from './hexToRgb';
 import hex2rgba, { hex2rgbaString, hex2rgbaObject, hex2rgbaArray } from './hexToRgba';
 import { OutputType } from './types';
+import hsl2Rgb, { hsl2RgbString, hsl2RgbObject, hsl2RgbArray } from './hslToRgb';
+import rgb2Hsl, { rgb2HslString, rgb2HslObject, rgb2HslArray } from './rgbToHsl';
 
 export {
     rgba2rgb,
@@ -17,4 +19,12 @@ export {
     hex2rgbaObject,
     hex2rgbaArray,
     OutputType,
+    hsl2Rgb,
+    hsl2RgbString,
+    hsl2RgbObject,
+    hsl2RgbArray,
+    rgb2Hsl,
+    rgb2HslString,
+    rgb2HslObject,
+    rgb2HslArray,
 };
