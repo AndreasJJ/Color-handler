748b53f981dd4ab6f50786b96bef09c2ccdcb69b
Andreasjj <andreasjj@gmail.com>
2020-10-21 23:08:32 +0200

    Added rgb2cmyk function

---
+++ src/rgb2cmyk/index.ts
@@ -0,0 +1,57 @@
+import { parse2rgb } from '../utilities/parse';
+import { CMYK, RgbTypes, OutputType, CmykTypes } from '../types';
+
+function rgb2cmyk(input: RgbTypes, output?: OutputType): CmykTypes {
+    switch (output) {
+        case OutputType.STRING: {
+            return rgb2cmykString(input);
+        }
+        case OutputType.OBJECT: {
+            return rgb2cmykObject(input);
+        }
+        case OutputType.ARRAY: {
+            return rgb2cmykArray(input);
+        }
+        default: {
+            return rgb2cmykObject(input);
+        }
+    }
+}
+
+function rgb2cmykString(input: RgbTypes): string {
+    const parsed = parse2rgb(input);
+    const cmyk = _rgb2cmyk(parsed.r, parsed.g, parsed.b);
+    return `cmyk(${cmyk.c}%, ${cmyk.m}%, ${cmyk.y}%, ${cmyk.k}%)`;
+}
+
+function rgb2cmykObject(input: RgbTypes): CMYK {
+    const parsed = parse2rgb(input);
+    return _rgb2cmyk(parsed.r, parsed.g, parsed.b);
+}
+
+function rgb2cmykArray(input: RgbTypes): [number, number, number, number] {
+    const parsed = parse2rgb(input);
+    const cmyk = _rgb2cmyk(parsed.r, parsed.g, parsed.b);
+    return [cmyk.c, cmyk.m, cmyk.y, cmyk.k];
+}
+
+function _rgb2cmyk(r: number, g: number, b: number): CMYK {
+    const rc = r / 255;
+    const gc = g / 255;
+    const bc = b / 255;
+    const k = 1 - Math.max(rc, gc, bc);
+
+    const c = (1 - rc - k) / (1 - k);
+    const m = (1 - gc - k) / (1 - k);
+    const y = (1 - bc - k) / (1 - k);
+
+    return {
+        c,
+        m,
+        y,
+        k,
+    };
+}
+
+export default rgb2cmyk;
+export { rgb2cmyk, rgb2cmykString, rgb2cmykObject, rgb2cmykArray };
--- src/types.ts
+++ src/types.ts
@@ -17,6 +17,13 @@ export interface HSL {
     l: number;
 }
 
+export interface CMYK {
+    c: number;
+    m: number;
+    y: number;
+    k: number;
+}
+
 export enum OutputType {
     STRING = 'string',
     OBJECT = 'object',
@@ -26,3 +33,4 @@ export enum OutputType {
 export type RgbaTypes = string | RGBA | [number, number, number, number];
 export type RgbTypes = string | RGB | [number, number, number];
 export type HslTypes = string | HSL | [number, number, number];
+export type CmykTypes = string | CMYK | [number, number, number, number];
